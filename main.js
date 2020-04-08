6c320d3841bd578da4febb3f5b4310437b7897fc
Gerardo Pagliarulo <gerardopagliarulo3@gmail.com>
2020-04-08 17:20:04 +0200

    Hamburger menu jQuery

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
 /**
- * HAMBURGER MENU
- * 
- * mostrare / nascondere il menu principale 
- * (ricordate che per vedere l’hamburger in alto a destra 
- * dovete scendere a risoluzioni tipo tablet o mobile).
- */
+* HAMBURGER MENU
+* 
+* mostrare / nascondere il menu principale 
+* (ricordate che per vedere l’hamburger in alto a destra 
+* dovete scendere a risoluzioni tipo tablet o mobile).
+*/
 
- var hamburger = $('.header-right > a');
- var croce = $('.hamburger-menu > a');
+var hamburger = $('.header-right > a');
+var croce = $('.hamburger-menu > a');
 
- hamburger.click( function () {
-     $('.hamburger-menu').fadeIn('fast');
- });
+hamburger.click( function () {
+    $('.hamburger-menu').fadeIn('fast');
+});
 
- croce.click( function () {
-     $('.hamburger-menu').fadeOut('fast');
- });
+croce.click( function () {
+    $('.hamburger-menu').fadeOut('fast');
+});
+
 
 
